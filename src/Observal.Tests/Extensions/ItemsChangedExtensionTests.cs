9cb3eeb2c4d1361a1a85b650265da3b62df6c723
paul <paul@localhost>
2010-08-08 21:27:02 +1000

    Added tests for ObserverExtension base class and ItemChangedExtension

---
+++ src/Observal.Tests/Extensions/ItemsChangedExtensionTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using Observal.Extensions;
+
+namespace Observal.Tests.Extensions
+{
+    [TestFixture]
+    public class ItemsChangedExtensionTests
+    {
+        [Test]
+        public void NotifiesOfAdds()
+        {
+            var added = new List<string>();
+
+            var observer = new Observer();
+            observer.Extend(new ItemsChangedExtension()).WhenAdded(x => added.Add((string)x));
+            observer.Add("Item 1");
+            observer.Add("Item 2");
+
+            Assert.AreEqual(2, added.Count);
+            Assert.AreEqual("Item 1", added[0]);
+            Assert.AreEqual("Item 2", added[1]);
+        }
+
+        [Test]
+        public void NotifiesOfRemoves()
+        {
+            var removed = new List<string>();
+
+            var observer = new Observer();
+            observer.Extend(new ItemsChangedExtension()).WhenRemoved(x => removed.Add((string)x));
+            observer.Add("Item 1");
+            observer.Add("Item 2");
+            observer.Release("Item 2");
+
+            Assert.AreEqual(1, removed.Count);
+            Assert.AreEqual("Item 2", removed[0]);
+        }
+
+        [Test]
+        public void NotifiesOfAddsAndRemoves()
+        {
+            var addedOrRemoved = new List<string>();
+            
+            var observer = new Observer();
+            observer.Extend(new ItemsChangedExtension()).WhenAddedOrRemoved(x => addedOrRemoved.Add((string)x));
+            observer.Add("Item 1");
+            observer.Add("Item 2");
+            observer.Release("Item 2");
+
+            Assert.AreEqual(3, addedOrRemoved.Count);
+            Assert.AreEqual("Item 1", addedOrRemoved[0]);
+            Assert.AreEqual("Item 2", addedOrRemoved[1]);
+            Assert.AreEqual("Item 2", addedOrRemoved[2]);
+        }
+    }
+}
---
+++ src/Observal.Tests/Extensions/ObserverExtensionTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Observal.Extensions;
+
+namespace Observal.Tests.Extensions
+{
+    [TestFixture]
+    public class ObserverExtensionTests
+    {
+        public class CounterExtension : ObserverExtension
+        {
+            public int Count { get; private set; }
+
+            public void SomeSetting()
+            {
+                AssertNotConfiguredYet("Should not be invoked after config.");
+            }
+
+            protected override void Attach(object attachedItem)
+            {
+                Count++;
+            }
+
+            protected override void Detach(object detachedItem)
+            {
+                Count--;
+            }
+        }
+
+        [Test]
+        public void CanUseAssertNotConfiguredYetToEnsureSettingsAreSetBeforeConfigure()
+        {
+            var ex = new CounterExtension();
+
+            var observer = new Observer();
+            observer.Extend(ex).SomeSetting();
+
+            observer.Add("Hello"); // Configured
+
+            Assert.Throws<InvalidOperationException>(ex.SomeSetting);
+        }
+
+        [Test]
+        public void EnsuresCannotBeConfiguredTwice()
+        {
+            var ex = new CounterExtension();
+
+            var observer = new Observer();
+            observer.Extend(ex).SomeSetting();
+
+            observer.Add("Hello"); // Configured
+
+            Assert.Throws<InvalidOperationException>(() => ((IObserverExtension)ex).Configure(new Observer()));
+        }
+    }
+}
